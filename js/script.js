4b9ae2cf7bd81d63c8b2acc044aa541935a98a32
Hashan Gunathilake <hashangunathilaka99@gmail.com>
2024-07-21 12:15:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,37 +37,12 @@ document.addEventListener("DOMContentLoaded", () => {
     //Conditional for the game
     if (player === computer) {
       result = "Tie";
-      displayresult.innerHTML = `<p class ='result-paragraph'> player ${player} <br> Computer  ${computer} <br> Result : Game ${result} </p>`;
-    } else if (player === "🤚") {
-      switch (computer) {
-        case "✌":
-          result = "lost";
-          break;
-
-        case "✊":
-          result = "win";
-          break;
-      }
-    } else if (player === "✌") {
-      switch (computer) {
-        case "🤚":
-          result = "win";
-          break;
-
-        case "✊":
-          result = "lost";
-          break;
-      }
-    } else if (player === "✊") {
-      switch (computer) {
-        case "✌":
-          result = "win";
-          break;
-
-        case "🤚":
-          result = "lost";
-          break;
-      }
+    } else if (
+      (player === "🤚" && computer === "✊") || (player === "✌" && computer === "🤚") || (player === "✊" && computer === "✌")
+    ) {
+      result = "win";
+    } else {
+      result = "lost";
     }
 
     if (result === "win") {
@@ -76,46 +51,39 @@ document.addEventListener("DOMContentLoaded", () => {
       score.computer += 10;
     }
     updatedom(player, computer, result);
-  
   }
   // updating the dom
-  function updatedom(player, computer, result){
+  function updatedom(player, computer, result) {
     displayresult.innerHTML = `<p class ='result-paragraph'> player ${player} <br> Computer ${computer} <br> Result : player  ${result} </p>`;
     displayscore.innerHTML = ` <p> Player : ${score.player} | ${score.computer} : Computer </P>`;
     gameresult();
-  };
+  }
 
   //finalizing the game score
-  function gameresult(){
-
-    if(score.player === 200 || score.computer === 200 ){
-
-        if(score.player > score.computer){
-
-          alert(`your score is ${score.player} so You Win the Game Congratulations...`);
-          displayresult.innerHTML = `<p class ='result-paragraph'> player 0 <br> Computer 0 </p>`;
-          displayscore.innerHTML = ` <p> Player : 0 | 0 : Computer </P>`;
-          score["player"] = 0;
-          score["computer"] = 0;
-        }
-        else if(score.player < score.computer){
-        
-          alert(`Computer score is ${score.computer} so Computer Win the Game`);
-          displayresult.innerHTML = `<p class ='result-paragraph'> player 0 <br> Computer 0 </p>`;
-          displayscore.innerHTML = ` <p> Player : 0 | 0 : Computer </P>`;
-          score["player"] = 0;
-          score["computer"] = 0;
-        }
-
+  function gameresult() {
+    if (score.player === 200 || score.computer === 200) {
+      if (score.player > score.computer) {
+        alert(
+          `your score is ${score.player} so You Win the Game Congratulations...`
+        );
+        displayresult.innerHTML = `<p class ='result-paragraph'> player 0 <br> Computer 0 </p>`;
+        displayscore.innerHTML = ` <p> Player : 0 | 0 : Computer </P>`;
+        score["player"] = 0;
+        score["computer"] = 0;
+      } else if (score.player < score.computer) {
+        alert(`Computer score is ${score.computer} so Computer Win the Game`);
+        displayresult.innerHTML = `<p class ='result-paragraph'> player 0 <br> Computer 0 </p>`;
+        displayscore.innerHTML = ` <p> Player : 0 | 0 : Computer </P>`;
+        score["player"] = 0;
+        score["computer"] = 0;
+      }
     }
-
-
-  };
+  }
 
   // clear game results
   function clearresults() {
     endgamebutton.addEventListener("click", () => {
-      displayresult.innerHTML = `<p class ='result-paragraph'> player 0 <br> Computer 0 </p>`;
+      displayresult.innerHTML = ``;
       displayscore.innerHTML = ` <p> Player : 0 | 0 : Computer </P>`;
       score["player"] = 0;
       score["computer"] = 0;
